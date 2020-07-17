caadb553749cd7b6da8b0f9fd7b191addf74f39f
its-tn10 <its.tn10@tamu.edu>
2020-07-17 10:31:23 +0200

    Demonstration with example data, configuration

--- database.sql
+++ database.sql
@@ -48,3 +48,29 @@ CREATE TABLE subscriptions (
     CONSTRAINT subscriptions_ibfk_1 FOREIGN KEY (client_id) REFERENCES clients (id) ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT subscriptions_ibfk_2 FOREIGN KEY (topic_id) REFERENCES topics (id) ON DELETE CASCADE ON UPDATE CASCADE
 );
+
+
+INSERT INTO clients (id, email, publisher, subscriber) VALUES
+    (1, 'tientavu@tamu.edu', FALSE, TRUE),
+    (2, 'tientavu@hotmail.com', FALSE, TRUE),
+    (3, 'advising@cse.tamu.edu', TRUE, FALSE),
+    (4, 'somebody@paycomonline.com', TRUE, FALSE),
+    (5, 'shipment-tracking@amazon.com', TRUE, FALSE);
+
+INSERT INTO topics (id, name) VALUES
+    (1, 'TAMUCSCSE'),
+    (2, 'PAYCOMONLINE'),
+    (3, 'AMAZONDELIVERY');
+
+INSERT INTO messages (id, topic_id, message) VALUES
+    (1, 1, 'Force requests for classes are currently at a halt.'),
+    (2, 3, 'Clean Code and The Phoenix Project have been shipped.'),
+    (3, 2, 'The Virtual Summer Engagement Program has kicked off!'),
+    (4, 1, 'A new, updated schedule for Fall 2020 classes is now up.'),
+    (5, 2, 'We are now approaching the end of the SEP program!');
+
+INSERT INTO subscriptions (client_id, topic_id) VALUES
+    (1, 1),
+    (1, 2),
+    (2, 1),
+    (2, 3);
